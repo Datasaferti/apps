8e094b9432a72858b03bc22683f66934a7a68d7b
Datasaferti <info@datasaferti.com>
2026-09-08 16:53:13 -0300

    Add PowerShell script to install DataSafer
    Script downloads and executes the DataSafer installer, handling errors and cleaning up temporary files.

---
+++ iniciar.ps1
@@ -0,0 +1,23 @@
+# Caminho do arquivo temporario
+ $urlInstalador = "https://raw.githubusercontent.com/Datasaferti/apps/main/instalador.ps1"
+ $arquivoTemp = "$env:TEMP\instalador_datasafer.ps1"
+
+Write-Host "Baixando o menu de instalacao DataSafer..." -ForegroundColor Cyan
+
+try {
+    # Baixa o instalador para a pasta TEMP
+    Invoke-WebRequest -Uri $urlInstalador -OutFile $arquivoTemp -UseBasicParsing
+
+    # Executa o instalador e espera ele ser encerrado (quando o usuario digitar 0)
+    & $arquivoTemp
+}
+catch {
+    Write-Host "Erro ao baixar ou executar o script. Verifique a conexao com a internet." -ForegroundColor Red
+}
+finally {
+    # Assim que o instalador fecha (opcao 0), apaga o arquivo da pasta TEMP
+    if (Test-Path $arquivoTemp) {
+        Remove-Item $arquivoTemp -Force
+        Write-Host "Arquivo temporario removido com sucesso do computador." -ForegroundColor Green
+    }
+}
